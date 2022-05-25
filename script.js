48cca004160d9c02abe7bae551b2b1dfe8044be2
Gabriel Rodrigues <99913525+brittola@users.noreply.github.com>
2022-05-25 10:50:40 -0300

    new function and bug fixed
    created function checkCases and fixed the case where only points and operators could be typed

--- script.js
+++ script.js
@@ -2,38 +2,44 @@ let screen = document.getElementById('outScreen');
 let buttons = document.getElementsByTagName('button');
 let point = false; //to check if there's already a point in the number
 
-function write(event){
+function write(event) {
     let btType = event.target.className;
     let btContent = event.target.textContent;
 
-    if(isEmpty() && btType == 'operator'){
+    checkCases(btType, btContent);
+}
+
+function checkCases(btType, btContent) {
+    if ((isEmpty() && btType == 'operator')
+        || (isEmpty() && btType == 'point')) {
         return;
-    }else if(btContent == 'C'){
+    } else if (btContent == 'C') {
         screen.value = '';
         point = false;
-    }else if(btContent == '='){
+    } else if (btContent == '=') {
         calcResult();
-    }else if(btType == 'operator' && isOperator()){
+    } else if (btType == 'operator' && isOperator()) { //if the last digit is an operator and an operator is typed, the last one is replaced
         screen.value = screen.value.slice(0, -1);
         screen.value += btContent;
-    }else if(btType == 'point' && !point){
+    } else if (btType == 'point' && !point && !isOperator()) { //a point will only be written if variable point is false and the last digit is not an operator
         screen.value += btContent;
         point = true;
-    }else if(btType == 'point' && point){
+    } else if ((btType == 'point' && point) || (btType == 'point' && isOperator())) {
         return;
-    }else{
+    } else {
         screen.value += btContent;
-        if(btType == 'operator'){
-            point = false;
+        
+        if (isOperator()) {
+            point = false; //if an operator is typed, the point variable is set to false
         }
     }
 }
 
 //check if the last digit is an operator
-function isOperator(){
+function isOperator() {
     let lastDigit = screen.value.slice(-1);
 
-    switch (lastDigit){
+    switch (lastDigit) {
         case '+':
             return true;
         case '-':
@@ -47,38 +53,40 @@ function isOperator(){
     }
 }
 
-function isEmpty(){
-    if(screen.value == ''){
+function isEmpty() {
+    if (screen.value == '') {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
-function calcResult(){
+function calcResult() {
     let written = screen.value;
 
-    if(written == ''){
+    if (written == '') {
         return;
+    } else if (written.charAt(written.length - 1) == '.' || isOperator()) {
+        written += 0;
     }
 
-    for(i in written){
-        if(written.charAt(i) == 'x'){
+    for (i in written) {
+        if (written.charAt(i) == 'x') {
             written = written.replace('x', '*');
-        }else if(written.charAt(i) == '÷'){
+        } else if (written.charAt(i) == '÷') {
             written = written.replace('÷', '/');
         }
     }
 
     screen.value = eval(written);
 
-    if(screen.value.indexOf('.') == -1){
+    if (screen.value.indexOf('.') == -1) {
         point = false;
-    }else{
+    } else {
         point = true;
     }
 }
 
-for(i = 0; i < buttons.length; i++){
+for (i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', write);
 }
